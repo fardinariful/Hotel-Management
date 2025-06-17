ef422b53811a8d2f77a23932ffa3f044ff06db6b
fardinariful <fardinewucse2020@gmail.com>
2025-06-17 16:26:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import {Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
+
+function App() {
+
+  const isOwnerPath=useLocation().pathname.includes("owner");
+  return (
+    <div>
+      {!isOwnerPath && <Navbar/>}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home></Home>}>
+             
+          </Route>
+
+        </Routes>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
